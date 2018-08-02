776f51b449950d88d3ff599b6ec45bb74d57be6e
Andrew Page <andrew@genesysit.net>
2018-08-02 16:32:18 +1000

    changed main display mode to flex

--- src/webparts/weatherWidget/components/ForecastDay/ForecastDay.tsx
+++ src/webparts/weatherWidget/components/ForecastDay/ForecastDay.tsx
@@ -18,12 +18,17 @@ export default class ForecastDay extends React.Component<IForecastDayProps, IFor
         return(
             <div className={ styles.dayContainer } style={{ gridColumn: this.props.key }}>
                 <h5 className={ styles.forecastDay }>{ this.props.day.day }</h5>
-                <Icon iconName={ this._getWeatherIcon(this.props.day.code) } className={ styles.weatherIcon }/>
-                <Icon iconName="CaretSolidUp" className={ styles.forecastHighIcon } />
-                <span className={ [styles.forecastTempText, styles.forecastTempTextHigh].join(" ") }>{ this.props.day.high }&deg;</span>
-                <Icon iconName="CaretSolidDown" className={ styles.forecastLowIcon } />
-                <span className={ [styles.forecastTempText, styles.forecastTempTextLow].join(" ") }>{ this.props.day.low }&deg;</span>
-
+                <div className={ styles.forecastIconContainer }>
+                    <Icon iconName={ this._getWeatherIcon(this.props.day.code) } className={ styles.weatherIcon }/>
+                    <div className={ styles.forecastTempIcons }>
+                        <Icon iconName="CaretSolidUp" className={ styles.forecastHighIcon } />
+                        <Icon iconName="CaretSolidDown" className={ styles.forecastLowIcon } />
+                    </div>
+                    <div className={ styles.forecastTempText }>
+                        <span className={ styles.forecastTempTextHigh }>{ this.props.day.high }&deg;</span>
+                        <span className={ styles.forecastTempTextLow }>{ this.props.day.low }&deg;</span>
+                    </div>
+                </div>
                 <span className={ styles.forecastText }>{ this.props.day.text }</span>
                 <h5 className={ styles.forecastDate }>{ this.props.day.date }</h5>
             </div>
@@ -33,8 +38,17 @@ export default class ForecastDay extends React.Component<IForecastDayProps, IFor
     private _getWeatherIcon(weatherCode: number){
         console.log(weatherCode)
         switch(weatherCode.toString()){
+            case "9":
+            case "39":
+            case "40": {
+                return "RainShowersDay"
+            }
             case "12": {
                 return "Rain"
+            }
+            case "24":
+            case "23":{
+                return "Duststorm"
             }
             case "32":
             case "33":
@@ -45,6 +59,12 @@ export default class ForecastDay extends React.Component<IForecastDayProps, IFor
             case "27": 
             case "28":{
                 return "Cloudy"
+            }
+            case "3":
+            case "4":
+            case "37":
+            case "38":{
+                return "Thunderstorms"
             }
             case "29":
             case "30":
